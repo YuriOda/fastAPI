940be11685ef7335b0c1af892be571c93bd06b06
Yuri <yuri.oda.lily@gmail.com>
2023-10-03 20:40:24 +0900

    add user

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
+# fastAPIのインスタンス化
 app = FastAPI()
 
-
-@app.get("/")
+# mainのURLにアクセスがあったら、下記の関数を処理する
+# @はdecorator
+@app.get("/") 
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/user/me")
+async def user_me():
+    return {"user_id": "current user"}
+
+# {}で囲むと引数として設定できる
+@app.get("/user/{user_id}")
+async def user_id(user_id: int):
+    return {"user_id": user_id}
+
+
